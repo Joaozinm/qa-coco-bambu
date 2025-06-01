dd2094877c36cfe28010df5c61016e168e813e4f
Joao Melo <vivitomelo@gmail.com>
2025-05-31 23:31:53 -0300

    Setup Cypress

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/features/**/*.feature",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({ plugins: [createEsbuildPlugin(config)] })
+      );
+      return config;
+    },
+  },
+});
